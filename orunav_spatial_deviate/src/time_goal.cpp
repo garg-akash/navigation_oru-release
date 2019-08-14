a4dd6cf0460048ca9fb67f054b247265c0755922
garg-akash <akashgrg2@gmail.com>
2019-08-14 15:37:13 +0530

    TimeToGoal addidtion

--- orunav_spatial_deviate/src/time_goal.cpp
+++ orunav_spatial_deviate/src/time_goal.cpp
@@ -35,9 +35,8 @@ void callback1(ros::NodeHandle &node_handle, const ControllerReportConstPtr& rep
 		orunav_generic::Path loaded_path_1 = orunav_generic::loadPathTextFile("/home/akash/catkin_myoru/path11.txt");
 		if (match(rep1, loaded_path_1)) {
 			node_handle.getParam("/bp_robot1_begin", bp_robot1_begin);
-			double goalTime_1 = ros::Time::now().toSec() - bp_robot1_begin; //TODO
-			cout<<"[TimeToGoal] for robot1 is : "<<goalTime_1<<" sec"<<endl;
-			cout<<ros::Time::now().toSec()<<"sec - "<<bp_robot1_begin<<"sec = "<<goalTime_1<<"sec"<<endl;
+			double goalTime_1 = ros::Time::now().toSec() - bp_robot1_begin; 
+			cout<<"[TimeToGoal] for robot1 is : "<<ros::Time::now().toSec()<<"sec - "<<bp_robot1_begin<<"sec = "<<goalTime_1<<"sec"<<endl;
 			node_handle.setParam("/bp_robot1", 0);
 		}
 	}
@@ -51,9 +50,8 @@ void callback2(ros::NodeHandle &node_handle, const ControllerReportConstPtr& rep
 		orunav_generic::Path loaded_path_2 = orunav_generic::loadPathTextFile("/home/akash/catkin_myoru/path22.txt");
 		if (match(rep2, loaded_path_2)) {
 			node_handle.getParam("/bp_robot2_begin", bp_robot2_begin);
-			double goalTime_2 = ros::Time::now().toSec() - bp_robot2_begin; //TODO
-			cout<<"[TimeToGoal] for robot2 is : "<<goalTime_2<<" sec"<<endl;
-			cout<<ros::Time::now().toSec()<<"sec - "<<bp_robot2_begin<<"sec = "<<goalTime_2<<"sec"<<endl;
+			double goalTime_2 = ros::Time::now().toSec() - bp_robot2_begin;
+			cout<<"[TimeToGoal] for robot2 is : "<<ros::Time::now().toSec()<<"sec - "<<bp_robot2_begin<<"sec = "<<goalTime_2<<"sec"<<endl;
 			node_handle.setParam("/bp_robot2", 0);
 		}
 	}
@@ -67,9 +65,8 @@ void callback3(ros::NodeHandle &node_handle, const ControllerReportConstPtr& rep
 		orunav_generic::Path loaded_path_3 = orunav_generic::loadPathTextFile("/home/akash/catkin_myoru/path33.txt");
 		if (match(rep3, loaded_path_3)) {
 			node_handle.getParam("/bp_robot3_begin", bp_robot3_begin);
-			double goalTime_3 = ros::Time::now().toSec() - bp_robot3_begin; //TODO
-			cout<<"[TimeToGoal] for robot3 is : "<<goalTime_3<<" sec"<<endl;
-			cout<<ros::Time::now().toSec()<<"sec - "<<bp_robot3_begin<<"sec = "<<goalTime_3<<"sec"<<endl;
+			double goalTime_3 = ros::Time::now().toSec() - bp_robot3_begin;
+			cout<<"[TimeToGoal] for robot3 is : "<<ros::Time::now().toSec()<<"sec - "<<bp_robot3_begin<<"sec = "<<goalTime_3<<"sec"<<endl;
 			node_handle.setParam("/bp_robot3", 0);
 		}
 	}		
